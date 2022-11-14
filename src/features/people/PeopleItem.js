644ac0d12a77ac1698922ed7042b3d189eeafa8e
Francesco Foglia <francescofogliajj@gmail.com>
2022-11-14 16:57:34 +0100

    modified:   public/index.html 	modified:   src/features/people/PeopleItem.js 	modified:   src/features/people/PeopleList.js 	new file:   src/features/people/PeopleSearch.js

--- src/features/people/PeopleItem.js
+++ src/features/people/PeopleItem.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 function PeopleItem({ item }) {
   return (
-    <h2 className="subtitle is-4">{ item.name }</h2>
+    <div className="column is-4">
+      <div className="card">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className='title is-4'>{ item.name }</p>
+              <p className='subtitle is-6'>Birth Date: { item.birth_year }</p>
+            </div>
+          </div>
+          <div className="content">
+            Mass: { item.mass }<br/>
+            Height: { item.height }<br/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/features/people/PeopleSearch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PeopleSearch(props) {
+  return (
+    <input className="input" type="text" placeholder="Luke Skywalker" />
+  )
+}
+
+export default PeopleSearch;
