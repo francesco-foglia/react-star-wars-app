b276ba7082847cf7aba8856b40830ab689c982c8
Francesco Foglia <francescofogliajj@gmail.com>
2022-11-14 17:38:45 +0100

    modified:   src/features/people/PeopleList.js 	modified:   src/features/people/peopleSlice.js

--- src/features/people/PeopleList.js
+++ src/features/people/PeopleList.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PeopleItem from "./PeopleItem";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchPeople, selectItems} from "./peopleSlice";
 
-function PeopleList({ people }) {
+function PeopleList() {
+  const people = useSelector(selectItems);
+  const dispatch = useDispatch();
+  /*
+  useEffect(() => {
+    dispatch(fetchPeople('https://swapi.dev/api/people/'))
+  });*/
+
   return (
     <div className="columns is-multiline">
       {
